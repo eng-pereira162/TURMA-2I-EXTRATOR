3c87af8dca6abbb2cbddd7252099e838963ea7b3
Lucas Pereira de Campos <eng.lucas.campos@usp.br>
2025-08-08 13:50:05 +0000

    FIM EXTRAIR

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
- const botaoExtrair = document.querySelector("#botao-palavrachave");
+const botaoExtrair = document.querySelector("#botao-palavrachave");
 
 
 
 botaoExtrair.addEventListener("click", apertarNoBotao);
 
 function apertarNoBotao() {
-    alert("VOU ESCREVER QUALQUER COISA")
+    const texto = document.querySelector("#entrada-de-texto").value;
+
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+
+    const palavras = texto.split(" ");
+
+    campoResultado.textContent = palavras.join(", ");
 
 }
+
